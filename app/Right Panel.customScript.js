8f541601144db8e4440b88d60f52b8feca64bfe5
nickolanack <nickblackwell82@gmail.com>
2017-05-23 11:37:00 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Right Panel.customScript.js
+++ app/Right Panel.customScript.js
@@ -1,5 +1,10 @@
+var viewer=new ContentModuleViewer(map, content, {});
+
 map.setMapitemSelectFn(function(mapitem) {
     
-    
+    viewer.open(new GeoliveTemplateModule(mapitem, {
+		template: "default",
+		page: "Detail"
+	}), mapitem);
     
 });
