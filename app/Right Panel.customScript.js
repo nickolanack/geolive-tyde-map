aed1c4c04c9624e8113446c5159c88e6c72619f5
nickolanack <nickblackwell82@gmail.com>
2017-09-06 15:12:49 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Right Panel.customScript.js
+++ app/Right Panel.customScript.js
@@ -4,6 +4,13 @@ var viewer=new ContentModuleViewer(map, container, {});
 
 var clearContent=function(){
     container.innerHTML="No item selected";
+    
+    viewer.open(new GeoliveTemplateModule(application, {
+		template: "default",
+		page: "EmptyView"
+	}), application);
+	show();
+    
 }
 sidePanel.addEvent('hide',function(){
     clearContent();
