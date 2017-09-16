cdc960f4c83c26ed8f25a71e1434330533989152
nickolanack <nickblackwell82@gmail.com>
2017-09-16 14:50:11 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.Configuration.102.formatParametersScript.js
+++ app/widget.Configuration.102.formatParametersScript.js
@@ -3,11 +3,18 @@ $parameters=array();
 foreach(MapController::GetMapsLayers(1) as $layer){
     $parameters[]=json_decode('{
         "type":"fieldset",
+        "label":"'.$layer->getId().' '.$layer->getName().' Cluster style",
         "fields":[{
             "type": "color",
             "name": "layer-'.$layer->getId().'-color",
             "default": "rgb(0,0,0)",
             "label": "Cluster Color"
+            },
+            {
+            "type": "color",
+            "name": "layer-'.$layer->getId().'-color-hover",
+            "default": "rgb(0,0,0)",
+            "label": "Cluster Hover Color"
             }
         ]}');
 }
