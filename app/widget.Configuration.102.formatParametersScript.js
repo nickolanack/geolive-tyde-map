b6016345133276e36e23a912afcc0f60e5b72cad
nickolanack <nickblackwell82@gmail.com>
2017-09-16 14:46:09 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.Configuration.102.formatParametersScript.js
+++ app/widget.Configuration.102.formatParametersScript.js
@@ -1,6 +1,15 @@
-return json_decode('[{
+GetPlugin('Maps');
+$parameters=array();
+foreach(MapController::GetMapsLayers(1) as $layer){
+    $parameters[]=json_decode('{
         "type": "color",
-        "name": "layer-25-color",
+        "name": "layer-'.$layer->getId().'-color",
         "default": "rgb(0,0,0)",
-        "label": "layer-25-color"
-    }]');
+        "label": "'.$layer->getId().' '.$layer->getId().' Cluster Color"
+    }');
+}
+
+
+
+
+return $parameters;
